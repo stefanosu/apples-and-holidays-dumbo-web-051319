f88cdc76784b7d187b893b9007b73b5a1d630e83
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-10 07:00:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,7 +53,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  holiday_hash[:winter][:christmas][:new_years]
+  holiday_hash[:winter][:christmas][0][1][:new_years][0]
   
   # return an array of all of the supplies that are used in the winter season
 
